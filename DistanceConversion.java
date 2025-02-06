78aff20543b6876b3cd0d3ccfa211ad4cd08f6c6
iamarnav0015 <shubhamsaxena2828@gmail.com>
2025-02-06 15:25:46 +0530

    Deleted unwanted files

--- DistanceConversion.java
+++
@@ -1,9 +0,0 @@
-public class DistanceConversion {
-    public static void main(String[] args) {
-        Double Distance = 10.8;
-        Double Miles = 1.6;
-        Double Result = Distance*Miles;
-
-        System.out.println("The DistanceKm In Miles is " + Result );
-    }
-}
