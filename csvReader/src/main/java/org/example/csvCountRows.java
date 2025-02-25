7afaf85cc06b9ee14dd04084d34399cfb341d421
Arnav@Victor <shubhamsaxena2828@gmail.com>
2025-02-25 23:43:12 +0530

    Task25

--- csvReader/src/main/java/org/example/csvCountRows.java
+++ csvReader/src/main/java/org/example/csvCountRows.java
@@ -4,18 +4,21 @@ import com.opencsv.CSVReader;
 
 import java.io.FileReader;
 
-public class ReadCsv {
+public class csvCountRows {
     public static void main(String[] args) {
-
         String path = "src/main/java/org/example/data.csv";
+        int rows = 0;
         try(CSVReader reader = new CSVReader(new FileReader(path))){
-            String[] file;
+            String[] file ;
+            reader.readNext();
             while ((file = reader.readNext()) != null){
-                System.out.println("Id:-" + file[0] + " Name :- " + file[1] + " Age:- " + file[2] + " Marks :- " + file[3]);
+                rows++;
             }
+            System.out.println("Total Number of Rows:- " + rows);
         }
         catch (Exception e){
             System.out.println(e.getMessage());
         }
+        
     }
 }
--- csvReader/src/main/java/org/example/csvFilterRecords.java
+++ csvReader/src/main/java/org/example/csvFilterRecords.java
@@ -4,15 +4,19 @@ import com.opencsv.CSVReader;
 
 import java.io.FileReader;
 
-public class ReadCsv {
+public class csvFilterRecords {
     public static void main(String[] args) {
-
         String path = "src/main/java/org/example/data.csv";
+
         try(CSVReader reader = new CSVReader(new FileReader(path))){
             String[] file;
+            reader.readNext();
             while ((file = reader.readNext()) != null){
-                System.out.println("Id:-" + file[0] + " Name :- " + file[1] + " Age:- " + file[2] + " Marks :- " + file[3]);
+                if (Integer.parseInt(file[3]) > 80){
+                    System.out.println("Id:-" + file[0] + " Name :- " + file[1] + " Age:- " + file[2] + " Marks :- " + file[3]);
+                }
             }
+
         }
         catch (Exception e){
             System.out.println(e.getMessage());
